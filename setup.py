0490fa61a2ac2d230c35dfc85586c2b773f5d29c
Alexander Grishchenko <alexanderdemure@gmail.com>
2025-04-26 13:02:57 +0500

    Initial Commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="gadignore",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     package_data={
         "gadignore": [".gitignore"],
