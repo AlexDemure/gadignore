f391cf64fe4682f17e1e6375ab2c5122ac40b7fc
Alexander Grishchenko <alexanderdemure@gmail.com>
2025-04-26 12:54:47 +0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# gadignore
+<p align="center">
+  <a href="https://github.com/AlexDemure/gadignore">
+    <a href="https://ibb.co/39sRV2np"><img src="https://i.ibb.co/Nd3Tf5M9/logo.png" alt="logo" border="0"></a>
+  </a>
+</p>
+
+<p align="center">
+  CLI tool for quickly generating a .gitignore file for Python projects.
+</p>
+
+---
+
+### Installation
+
+```
+pip install gadignore
+```
+
+### Usage
+
+```sh
+gadignore
+```
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="gadignore",
+    version="0.0.1",
+    packages=find_packages(),
+    package_data={
+        "gadignore": [".gitignore"],
+    },
+    install_requires=requirements,
+    entry_points={},
+    author="Alexander Grishchenko",
+    author_email="alexanderdemure@gmail.com",
+    description="CLI tool for quickly generating a .gitignore file for Python projects",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlexDemure/gadignore",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
